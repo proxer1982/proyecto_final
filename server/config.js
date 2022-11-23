8eb8c53c884d9e12624ab1ed3e660d2cfda2cdfd
proxer1982 <juanc.zorro@gmail.com>
2022-11-22 21:15:03 -0500

    v2

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://developers:123Rr@clusterprinc.crxbi6b.mongodb.net/developers3x1?retryWrites=true&w=majority"
-export const PORT = process.env.PORT || 5050
+export const PORT = process.env.PORT || 8080
 export const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME || "developers3x1"
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || 982138118491583
 export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || "BIbOGuY9OdDwBybKVa1vOfnXdvM"
