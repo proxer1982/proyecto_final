f58a59a89670f987488d3b12376d70684899cbbc
proxer1982 <juanc.zorro@gmail.com>
2022-11-22 22:08:17 -0500

    v8

--- src/src/constantes.js
+++ src/src/constantes.js
@@ -1,8 +1,4 @@
-import dotenv from 'dotenv' 
-
-dotenv.config()
-
-const PORT = process.env.PORT || 8080
+const PORT = 8080
 
 const srvBck = "http://localhost:" + PORT
 const srvFrt = "http://localhost:" + PORT
